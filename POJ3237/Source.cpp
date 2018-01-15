f7bbc7f74b38a26f0f2da798030117b3877c727a
Leaf <Leaf@DESKTOP-DDP68PJ>
2018-01-15 18:14:58 +0800

    To solve the problem

--- POJ3237/Source.cpp
+++ POJ3237/Source.cpp
@@ -1,42 +1,102 @@
-#include<cstdio>
 #include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<cstdlib>
 #include<string>
 
 using namespace std;
 
 int main()
 {
-	string expression;
+	int value[26];
+	int occur[26];
+	int result;
+	int len;
+	int i;
+
 	int nCase;
+	char originStr[10000] = { 0 };
+	char expStr[10000];
+	string exp;
 
-	int result, value[26] = { 0 };
-	bool occur[26] = { 0 };
-	int i, j;
-	char varChar;
+	int factor;
 
-	while (cin >> nCase)
+	cin >> nCase;
+	getchar();
+
+	while (nCase--)
 	{
-		while (nCase--)
+		getline(cin, exp);
+		memcpy(originStr, exp.c_str(), exp.length());
+		len = 0;
+
+		for (i = 0;i < exp.length();i++)
 		{
-			getline(cin, expression);
+			if (originStr[i] != ' ')
+				expStr[len++] = originStr[i];
+		}
 
+		for (i = 0;i < 26;i++)
+		{
+			value[i] = i + 1;
+			occur[i] = 0;
+		}
 
+		result = 0;
+		i = 0;
+		factor = 1;
 
-
-
-			cout << "Expression: " << expression << endl;
-			cout << "value: " << result << endl;
-			for (i = 0;i < 26;i++)
+		while (i < len)
+		{
+			if (expStr[i] == '+' || expStr[i] == '-')
 			{
-				if (occur[i])
+				if (expStr[i] == '+'&&expStr[i + 1] == '+')
 				{
-					varChar = 'a' + i;
-					cout << varChar << " = " << value[i] << endl;
+					i += 2;
+					value[expStr[i] - 'a'] += 1;
 				}
+				else if (expStr[i] == '-'&&expStr[i + 1] == '-')
+				{
+					i += 2;
+					value[expStr[i] - 'a'] -= 1;
+				}
+				else
+				{
+					factor = (expStr[i] == '+' ? 1 : -1);
+					i++;
+				}
+			}
+			else if (isalpha(expStr[i]))
+			{
+				occur[expStr[i] - 'a'] = 1;
+				result += factor*value[expStr[i] - 'a'];
 
+				if (expStr[i + 1] == '+'&&expStr[i + 2] == '+')
+				{
+					value[expStr[i] - 'a'] += 1;
+					i += 2;
+				}
+				else if (expStr[i + 1] == '-'&&expStr[i + 2] == '-')
+				{
+					value[expStr[i] - 'a'] -= 1;
+					i += 2;
+				}
+				else
+					i++;
+			}
+		}
+
+		cout << "Expression: " << exp << endl;
+		cout << "value = " << result << endl;
+		for (i = 0;i < 26;i++)
+		{
+			if (occur[i])
+			{
+				cout << char(i + 'a') << " = " << value[i] << endl;
 			}
 		}
 	}
 
+	return 0;
 
 }
